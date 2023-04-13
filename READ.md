5232211b125386936d1bcdf3ca373f1ac5f6fd43
THPchristopheF <chrisbymail@gmail.com>
2023-04-13 20:32:26 +0200

    big data-block chain

---
+++ READ.md
@@ -0,0 +1,16 @@
+00_journalists.rb
+
+    Combien y a-t-il de handle dans cette array ?
+    Quel est le handle le plus court de cette liste ?
+    Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)
+    Combien commencent par une majuscule (première lettre juste après le @) ?
+    Trie la liste de handle par ordre alphabétique.
+    Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)
+    Quelle est la position dans l'array de la personne @epenser ?
+    Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)
+
+01_cryptocurrencies.rb
+    La ou les crypto qui ont la plus grosse valeur.
+    La ou les crypto qui ont la plus petite valeur.
+    Les devises dont le cours est inférieur à 6000
+    La devise la plus chère parmi celles dont le cours est inférieur à 6000.
---
+++ lib/00_journalists.rb
@@ -0,0 +1,124 @@
+
+#
+#compte le nombre de comptes
+#
+def handlecounter(an_array)
+count=an_array.count
+return count
+end 
+
+#trouve le compte le plus court
+def shorther_handle(an_array)
+tab_size=handlecounter(an_array)
+short_size=10000000
+short_size_id=0
+ 0.upto(tab_size-1) { |i|
+   if (an_array.at(i).length)<short_size then 
+       short_size=(an_array.at(i).length)
+       short_size_id=i 
+   end}
+ 
+ return an_array.at(short_size_id)
+  end
+
+  #
+  # compte le nbre de handle avec x char
+  #
+def with_n_char(an_array , fixed_lenght)
+
+  tab_size=handlecounter(an_array)
+  counter=0
+  fixed_lenght=fixed_lenght+1  #+1 car on ne compte pas @
+   0.upto(tab_size-1) { |i|
+     if (an_array.at(i).length)==fixed_lenght then 
+         counter=counter+1
+        # puts "#{i} #{an_array.at(i)} lenght=#{an_array.at(i).length}" 
+     end}
+    return counter
+  end
+
+
+   #
+   #  compte le nbre handle avec la 1ere lettre en majuscule
+   #
+  def count_first_letter_in_uppercase(an_array)
+    tab_size=handlecounter(an_array)
+    counter=0
+    0.upto(tab_size-1) { |i|
+       if ((an_array.at(i))[1,1])=~ /[A-Z]/ then  # [1,1] a partir du 2eme char ,je recup 1 char
+           counter=counter+1
+         #  puts "#{i} #{an_array.at(i)} -------- first char=#{an_array.at(i)[1,1]}" 
+       end}
+     
+     return counter
+    end
+
+
+def alphabetical_order (an_array) 
+    tab=an_array.sort
+    display(tab)
+    return 
+end
+
+
+
+def sortby_string_lenght_asc(an_array)
+#tab=an_array.sort_by {|str| str.length}
+tab=an_array.sort_by(&:length)
+display(tab)
+return 
+end
+
+def display (an_array)
+  tab_size=handlecounter(an_array)
+  0.upto(tab_size-1) { |i|
+      puts "#{i} #{an_array.at(i)} " 
+}
+end
+  
+  
+def countbylength (an_array)
+maxlength=(an_array.max_by(&:length)).length #trouve la longueur du compte le plus long dans  le tableau 
+#puts "#{maxlength}"
+1.upto(maxlength) { |i|
+count=an_array.select {|account| account.length==i}.length #count est egale au nbre de compte de longueur i
+puts "nb char :#{i} nbre de compte: #{count}"
+}
+return 
+end
+
+######### programme principale #########
+
+def perform
+my_array=["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+   # methode handlecounter(an_array) pour compter les handles
+count=handlecounter(my_array)
+puts "Le nombre de compte est #{count}"
+
+## trouve le handle le plus court
+shorter_id=shorther_handle(my_array)
+puts "le plus court handle est #{shorter_id}"
+
+   # compte le nbre de handle avec 5 caractere (@ est exclu)
+
+puts "Le nombre de handle avec 5 caracteres est :  #{with_n_char(my_array, 5)}"
+   #  compte le nbre handle avec la 1ere lettre en majuscule
+puts "Le nombre de handle avec le premier caractere en majuscule est: #{count_first_letter_in_uppercase(my_array)}"
+
+
+#  tri par order alphabetique 
+puts "alphabetical order :" 
+puts "#{alphabetical_order(my_array)}"
+
+
+#  tri ascendant par nombre de caractere" 
+puts "tri ascendant par nombre de caractere"
+puts "#{sortby_string_lenght_asc(my_array)}"
+
+puts "repartition par nombre de caractere"
+puts "#{countbylength(my_array)}"
+end
+
+perform
+
+
